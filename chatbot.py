04bb14c07852c51f72b133b1acbdc319b7986564
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-01-26 17:16:59 -0300

    Imported database to python

--- chatbot.py
+++ chatbot.py
@@ -3,3 +3,13 @@
 # Importação das bibliotecas
 import numpy as np
 import tensorflow as tf
+import re
+import time
+
+# ---Parte 1 - pré-processamento dos dados -----
+
+# Importação das bases de dados
+
+linhas = open('dataset/movie_lines.txt', encoding=('utf-8'), errors=('ignore')).read().split('\n')
+conversas = open('dataset/movie_conversations.txt', encoding=('utf-8'), errors=('ignore')).read().split('\n')
+
