71cb7bf5aa62896952351917dde3b96e20b57d0c
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-01-26 17:33:51 -0300

    Created dictionary with each line ID

--- chatbot.py
+++ chatbot.py
@@ -13,3 +13,12 @@ import time
 linhas = open('dataset/movie_lines.txt', encoding=('utf-8'), errors=('ignore')).read().split('\n')
 conversas = open('dataset/movie_conversations.txt', encoding=('utf-8'), errors=('ignore')).read().split('\n')
 
+# Criação de um dicionário para mapear cada linha com seu ID
+id_para_linha = {}
+for linha in linhas:
+    # print(linha)
+    _linha = linha.split(' +++$+++ ')
+    # print(_linha)
+    if len(_linha) == 5:
+        # print(_linha[4])
+        id_para_linha[_linha[0]] = _linha[4]
