a91fc5bacb43c8f377e2e8a17433adc645514d5b
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-01-29 20:01:47 -0300

    Implemented function to count words in asks and answers. Improved clean text function

--- chatbot.py
+++ chatbot.py
@@ -46,21 +46,37 @@ for conversa in conversas_id:
 def limpa_texto(texto):
     texto = texto.lower()
     texto = re.sub(r"i'm", "i am", texto)
+    texto = re.sub(r"you're", "you are", texto)
     texto = re.sub(r"he's", "he is", texto)
     texto = re.sub(r"she's", "she is", texto)
-    texto = re.sub(r"that´s", "that is", texto)
+    texto = re.sub(r"it's", "it is", texto)
+    texto = re.sub(r"we're", "we are", texto)
+    texto = re.sub(r"they're", "they are", texto)
+    texto = re.sub(r"that's", "that is", texto)
     texto = re.sub(r"what's", "what is", texto)
     texto = re.sub(r"where's", "where is", texto)
-    texto = re.sub(r"/'ll", "will", texto)
-    texto = re.sub(r"/'ve", "have", texto)
-    texto = re.sub(r"/'re", "are", texto)
-    texto = re.sub(r"/'d", "would", texto)
+    texto = re.sub(r"'ll", " will", texto)
+    texto = re.sub(r"'ve", " have", texto)
+    texto = re.sub(r"'re", " are", texto)
+    texto = re.sub(r"'d", " would", texto)
+    texto = re.sub(r"isn't", "is not", texto)
+    texto = re.sub(r"aren't", "are not", texto)
     texto = re.sub(r"won't", "will not", texto)
-    texto = re.sub(r"cant't", "can not", texto)
-    texto = re.sub(r"[-()#/@;:<>{}~+=?.|,*]", "", texto)
+    texto = re.sub(r"can't", "can not", texto)
+    texto = re.sub(r"don't", "do not", texto)
+    texto = re.sub(r"doesn't", "does not", texto)
+    texto = re.sub(r"hasn't", "has not", texto)
+    texto = re.sub(r"haven't", "have not", texto)
+    texto = re.sub(r"wasn't", "was not", texto)
+    texto = re.sub(r"weren't", "were not", texto)
+    texto = re.sub(r"didn't", "did not", texto)
+    texto = re.sub(r"wouldn't", "would not", texto)
+    texto = re.sub(r"there's", "there is", texto)
+    texto = re.sub(r"there're", "there are", texto)
+    texto = re.sub(r"[-()#/@;:<>{}~+=?!.|,*]", "", texto)
     return texto
     
-limpa_texto("Exemplo i'm**")
+limpa_texto("Exemplo i'm** that's we'd")
 
 # Limpeza de perguntas
 perguntas_limpas = []
@@ -72,8 +88,24 @@ respostas_limpas = []
 for resposta in respostas:
     respostas_limpas.append(limpa_texto(resposta))
 
-
-
+# Criação de um dicionário que mapeie cada palavra e o número de ocorrências: pode-se
+# user a biblioteca NLTK
+palavras_contagem = {}
+for pergunta in perguntas_limpas:
+    # print(pergunta)
+    for palavra in pergunta.split():
+        if palavra not in palavras_contagem:
+            palavras_contagem[palavra] = 1
+        else:
+            palavras_contagem[palavra] += 1
+            
+for resposta in respostas_limpas:
+    # print(pergunta)
+    for palavra in resposta.split():
+        if palavra not in palavras_contagem:
+            palavras_contagem[palavra] = 1
+        else:
+            palavras_contagem[palavra] += 1
 
 
 
