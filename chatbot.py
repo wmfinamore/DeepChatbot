45c1d7bf62934563debc3d32c2bb78edb6e371da
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-01-30 10:36:33 -0300

    Added tokens in the dictionaries

--- chatbot.py
+++ chatbot.py
@@ -124,6 +124,17 @@ for  palavra, contagem in palavras_contagem.items():
         respostas_palavras_int[palavra] = numero_palavra
         numero_palavra += 1
 
+# Adição de tokens no dicionário
+tokens= ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
+for token in tokens:
+    perguntas_palavras_int[token] = len(perguntas_palavras_int) + 1
+    
+for token in tokens:
+    respostas_palavras_int[token] = len(respostas_palavras_int) + 1
+
+
+
+
 
     
 
